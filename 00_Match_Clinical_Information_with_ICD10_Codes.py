a5391dc7f7a0242a405a6a2a9ba9d226ef8e38a1
Kyoungsuk Park <bluemk00@gmail.com>
2024-12-11 11:25:03 +0900

    Update 00_Match_Clinical_Information_with_ICD10_Codes.py

--- 00_Match_Clinical_Information_with_ICD10_Codes.py
+++ 00_Match_Clinical_Information_with_ICD10_Codes.py
@@ -1,8 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-Author: Kyoungsuk Park
-Date: 2024-12-10
+MIT License
+
+Copyright (c) 2024 [Your Name]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
 Description:
     This script processes clinical data from `Clinical_Information.csv` and matches the `dx` column
     (diagnostic labels) with standardized ICD-10 codes provided in `vitaldb_dx_icd10_match.csv`.
